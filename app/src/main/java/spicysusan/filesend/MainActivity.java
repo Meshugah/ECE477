494c89e4344e76e8a5c4c3b61dab524ed52ceb72
Vignesh Karthikeyan <vkarthi@purdue.edu>
2017-09-12 22:42:05 -0400

    I think NFC tag detection works but need to reporgram tag

--- app/src/main/java/spicysusan/filesend/MainActivity.java
+++ app/src/main/java/spicysusan/filesend/MainActivity.java
@@ -1,5 +1,7 @@
 package spicysusan.filesend;
 
+import android.content.Intent;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,16 +10,42 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "NfcDemo";
+
+    private TextView mTextView;
+    private NfcAdapter mNfcAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.textView_explanation);
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if (mNfcAdapter == null) {
+            //If No NFC this should fail
+            Toast.makeText(this, "This does not suppport NFC", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        if (!mNfcAdapter.isEnabled()) {
+            mTextView.setText("NFC is disabled.");
+        } else {
+            mTextView.setText("NFC is enabled?");
+        }
+        handleIntent(getIntent());
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 //        Firebase
@@ -38,6 +66,12 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
+    }
+
+    private void handleIntent(Intent intent){
+        //TODO: handle it
     }
 
     @Override
